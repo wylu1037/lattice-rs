d7604bea5e0b0220bdd3aa2ed1c1507101c17416
wylu1037 <wylu1037@gmail.com>
2024-04-19 08:08:00 +0800

    feat: sign message

--- crypto/src/sign.rs
+++ crypto/src/sign.rs
@@ -3,7 +3,7 @@ use libsm::sm2::signature::SigCtx;
 use libsm::sm3::hash::Sm3Hash;
 use num_bigint::BigUint;
 use once_cell::sync::Lazy;
-use secp256k1::{All, PublicKey, rand::rngs::OsRng, Secp256k1, SecretKey};
+use secp256k1::{All, Message, PublicKey, rand::rngs::OsRng, Secp256k1, SecretKey};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Cryptography {
@@ -52,6 +52,9 @@ impl KeyPair {
         }
     }
 
+    /// 从私钥恢复密钥对
+    /// bytes 私钥
+    /// cryptography Secp256k1 or Sm2p256v1
     pub fn from_secret_key(bytes: &[u8], cryptography: Cryptography) -> KeyPair {
         match cryptography {
             Cryptography::Secp256k1 => {
@@ -73,6 +76,37 @@ impl KeyPair {
                     secret_key,
                     cryptography,
                 }
+            }
+        }
+    }
+
+    pub fn sign(&self, message: &[u8]) -> String {
+        match self.cryptography {
+            Cryptography::Secp256k1 => {
+                let sk = SecretKey::from_slice(&self.secret_key.to_bytes_be()).unwrap();
+                let msg = Message::from_digest_slice(&message).unwrap();
+                let (recovery_id, sig) = CONTEXT_SECP256K1
+                    .sign_ecdsa_recoverable(&msg, &sk).serialize_compact();
+                let r: &[u8] = &sig[..32];
+                let s: &[u8] = &sig[32..];
+                format!(
+                    "0x{}{}0{}",
+                    hex::encode(r),
+                    hex::encode(s),
+                    BigUint::from(recovery_id.to_i32() as u32).to_str_radix(16),
+                )
+            }
+            Cryptography::Sm2p256v1 => {
+                let pk = CURVE_SM2P256V1.bytes_to_point(&self.public_key).unwrap();
+                // Get the value "e", which is the hash of message and ID, EC parameters and public key
+                let digest = CONTEXT_SM2P256V1.hash("1234567812345678", &pk, message).unwrap();
+                let sig = CONTEXT_SM2P256V1.sign_raw(&digest[..], &self.secret_key).unwrap();
+                format!(
+                    "0x{}{}01{}",
+                    sig.get_r().to_str_radix(16),
+                    sig.get_s().to_str_radix(16),
+                    hex::encode(digest),
+                )
             }
         }
     }
@@ -119,5 +153,33 @@ mod tests {
         let expected = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824";
         let actual = hash_message(b"hello", Cryptography::Secp256k1);
         assert_eq!(actual, expected);
+    }
+
+    #[test]
+    fn sign_secp256k1() {
+        let sk =
+            hex::decode("c842e1ef9ece7e992a4021423a58d6e89c751881e43fd7dbebe70f932ad493e2").unwrap();
+
+        let data =
+            hex::decode("790dcb1e43ac151998f8c2e59e0959072f9d476d19fb6f98d7a4e59ea5f8e59e").unwrap();
+
+        let key_pair = KeyPair::from_secret_key(&sk, Cryptography::Secp256k1);
+
+        let sig = KeyPair::sign(&key_pair, &data);
+        assert_eq!(sig, "0xc8eced818b011433b5d486f9f0c97c8d0180a0df042bcaf1e75a7cd20d66920a5bbc4901bd90353fc62828ed2a821a801440f294779fc402033bf92c7657c30600");
+    }
+
+    #[test]
+    fn sign_sm2p256v1() {
+        let sk =
+            hex::decode("29d63245990076b0bbb33f7482beef21855a8d2197c8d076c2356c49e2a06322").unwrap();
+
+        let data =
+            hex::decode("790dcb1e43ac151998f8c2e59e0959072f9d476d19fb6f98d7a4e59ea5f8e59e").unwrap();
+
+        let key_pair = KeyPair::from_secret_key(&sk, Cryptography::Sm2p256v1);
+
+        let sig = KeyPair::sign(&key_pair, &data);
+        assert_eq!(sig, "7ca02541f80886a0612ca05bf555a1092a888abe3a6e0674381fc6fbf5253cea24e4ea409d7833868176d8eb01c852f841024a728f35a697ec9d691d6b5e24dd015931ab5708c28403560b471e30e7f5c404bdeabea2e8e2d5d6cc4f1ca96ba4aa");
     }
 }
