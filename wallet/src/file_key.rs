098f06efdc49e840431919b4bcde33a94a66ad07
wylu1037 <wylu1037@gmail.com>
2024-06-24 08:58:56 +0800

    feat: decrypt file_key

--- wallet/src/file_key.rs
+++ wallet/src/file_key.rs
@@ -92,6 +92,24 @@ impl FileKey {
             is_gm: matches!(cryptography, Cryptography::Sm2p256v1),
         }
     }
+
+    /// # 解密 FileKey
+    ///
+    /// ## 入参
+    /// + `password: &str`: 密码
+    ///
+    /// ## 出参
+    /// + `KeyPair`: 密钥对
+    fn decrypt(&self, password: &str) -> KeyPair {
+        let key = scrypt_key(password.as_bytes(), &self.cipher.kdf.kdf_params.salt);
+        let aes_key = hex::decode(&key[0..32]).unwrap();
+        let iv_bytes = hex::decode(&self.cipher.aes.iv).unwrap();
+        let str = aes::decrypt(&self.cipher.cipher_text, &aes_key, &iv_bytes);
+        let secret_bytes = hex::decode(str).unwrap();
+
+        let cryptography = if self.is_gm { Cryptography::Sm2p256v1 } else { Cryptography::Secp256k1 };
+        KeyPair::from_secret_key(&secret_bytes, cryptography)
+    }
 }
 
 /// # 生成 Cipher
@@ -187,5 +205,13 @@ mod tests {
         let json = r#"{"uuid":"0c9b1af7-23e4-4552-8d5b-cda4087a7779","address":"zltc_Z1pnS94bP4hQSYLs4aP4UwBP9pH8bEvhi","cipher":{"aes":{"cipher":"aes-128-ctr","iv":"1d1e3b6c9d8fdb254625888e5675cd1b"},"kdf":{"kdf":"scrypt","kdfParams":{"DKLen":32,"n":262144,"p":1,"r":8,"salt":"c55360cb01d2ea31f4a87eafad9b3254ec8c32a15f32ea985507121599066284"}},"cipherText":"c6ca9c0fbb08ad4546c68304100620a2f2cd2db9e80dd9c9f9d2425dab0cfbaa","mac":"fbaea48aaa56d5829f7e245f9efe397cab19d448560f8077e6324e16a07e9758"},"isGM":true}"#;
         let file_key = FileKey::new(json);
         assert_eq!(file_key.address, "zltc_Z1pnS94bP4hQSYLs4aP4UwBP9pH8bEvhi")
+    }
+
+    #[test]
+    fn test_decrypt_file_key() {
+        let json = r#"{"uuid":"0c9b1af7-23e4-4552-8d5b-cda4087a7779","address":"zltc_Z1pnS94bP4hQSYLs4aP4UwBP9pH8bEvhi","cipher":{"aes":{"cipher":"aes-128-ctr","iv":"1d1e3b6c9d8fdb254625888e5675cd1b"},"kdf":{"kdf":"scrypt","kdfParams":{"DKLen":32,"n":262144,"p":1,"r":8,"salt":"c55360cb01d2ea31f4a87eafad9b3254ec8c32a15f32ea985507121599066284"}},"cipherText":"c6ca9c0fbb08ad4546c68304100620a2f2cd2db9e80dd9c9f9d2425dab0cfbaa","mac":"fbaea48aaa56d5829f7e245f9efe397cab19d448560f8077e6324e16a07e9758"},"isGM":true}"#;
+        let file_key = FileKey::new(json);
+        let key_pair = file_key.decrypt("Root1234");
+        assert_eq!("23d5b2a2eb0a9c8b86d62cbc3955cfd1fb26ec576ecc379f402d0f5d2b27a7bb", hex::encode(key_pair.secret_key.to_bytes_be()))
     }
 }
