8f7187d0a79aa47155ff6eea6e748c91c7aa2a40
wylu1037 <wylu1037@gmail.com>
2024-07-12 19:54:12 +0800

    chore: decrypt file key

--- wallet/src/file_key.rs
+++ wallet/src/file_key.rs
@@ -8,6 +8,7 @@ use crypto::aes;
 use crypto::hash::hash_message;
 use crypto::sign::KeyPair;
 use model::Cryptography;
+use model::Error;
 use model::HexString;
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -100,15 +101,22 @@ impl FileKey {
     ///
     /// ## 出参
     /// + `KeyPair`: 密钥对
-    fn decrypt(&self, password: &str) -> KeyPair {
+    fn decrypt(&self, password: &str) -> Result<KeyPair, Error> {
         let key = scrypt_key(password.as_bytes(), &self.cipher.kdf.kdf_params.salt);
         let aes_key = hex::decode(&key[0..32]).unwrap();
-        let iv_bytes = hex::decode(&self.cipher.aes.iv).unwrap();
-        let str = aes::decrypt(&self.cipher.cipher_text, &aes_key, &iv_bytes);
-        let secret_bytes = hex::decode(str).unwrap();
 
+        let hash_key = hex::decode(&key[32..64]).unwrap();
         let cryptography = if self.is_gm { Cryptography::Sm2p256v1 } else { Cryptography::Secp256k1 };
-        KeyPair::from_secret_key(&secret_bytes, cryptography)
+        let actual_mac = compute_mac(&hash_key, &self.cipher.cipher_text, cryptography);
+        if !actual_mac.eq(&self.cipher.mac) {
+            return Err(Error::new("根据密码无法解析出私钥，请检查密码"));
+        }
+
+        let iv_bytes = hex::decode(&self.cipher.aes.iv).unwrap();
+        let sk_hex = aes::decrypt(&self.cipher.cipher_text, &aes_key, &iv_bytes);
+        let secret_bytes = hex::decode(sk_hex).unwrap();
+
+        Ok(KeyPair::from_secret_key(&secret_bytes, cryptography))
     }
 }
 
@@ -122,7 +130,7 @@ impl FileKey {
 /// + `Cipher`: struct
 fn gen_cipher(secret_key: &[u8], password: &[u8], cryptography: Cryptography) -> Cipher {
     let salt = hex::encode(random::<[u8; 32]>());
-    let iv_bytes = random::<[u8; 16]>();
+    let iv_bytes = random::<[u8; 16]>(); // 16 equals aes.BlockSize
     let iv = hex::encode(iv_bytes);
     let key = scrypt_key(password, &salt);
     let aes_key = hex::decode(&key[0..32]).unwrap();
@@ -138,7 +146,7 @@ fn gen_cipher(secret_key: &[u8], password: &[u8], cryptography: Cryptography) ->
             kdf: "scrypt".to_string(),
             kdf_params: KdfParams {
                 dk_len: 32,
-                n: 262144,
+                n: 262144, // 1<<18
                 p: 1,
                 r: 8,
                 salt,
@@ -211,7 +219,7 @@ mod tests {
     fn test_decrypt_file_key() {
         let json = r#"{"uuid":"0c9b1af7-23e4-4552-8d5b-cda4087a7779","address":"zltc_Z1pnS94bP4hQSYLs4aP4UwBP9pH8bEvhi","cipher":{"aes":{"cipher":"aes-128-ctr","iv":"1d1e3b6c9d8fdb254625888e5675cd1b"},"kdf":{"kdf":"scrypt","kdfParams":{"DKLen":32,"n":262144,"p":1,"r":8,"salt":"c55360cb01d2ea31f4a87eafad9b3254ec8c32a15f32ea985507121599066284"}},"cipherText":"c6ca9c0fbb08ad4546c68304100620a2f2cd2db9e80dd9c9f9d2425dab0cfbaa","mac":"fbaea48aaa56d5829f7e245f9efe397cab19d448560f8077e6324e16a07e9758"},"isGM":true}"#;
         let file_key = FileKey::new(json);
-        let key_pair = file_key.decrypt("Root1234");
+        let key_pair = file_key.decrypt("Root1234").unwrap();
         assert_eq!("23d5b2a2eb0a9c8b86d62cbc3955cfd1fb26ec576ecc379f402d0f5d2b27a7bb", hex::encode(key_pair.secret_key.to_bytes_be()))
     }
 }
