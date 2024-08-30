7d496fa44b833001226b192e4c30de2d67a59b84
wylu1037 <wylu1037@gmail.com>
2024-08-30 21:42:28 +0800

    feat: transfer

--- lattice/src/lattice.rs
+++ lattice/src/lattice.rs
@@ -11,11 +11,12 @@ use model::constants::{PREFIX_OF_HEX, ZERO_HASH_STRING, ZERO_ZLTC_ADDRESS};
 use model::receipt::Receipt;
 use wallet::file_key::FileKey;
 
-use crate::builder::{CallContractBuilder, DeployContractBuilder, TransactionBuilder};
+use crate::builder::{CallContractBuilder, DeployContractBuilder, TransactionBuilder, TransferBuilder};
 use crate::client::HttpClient;
 use crate::constants::REGEX_PRIVATE_KEY;
 
 /// 链配置
+#[derive(Debug, Clone, Copy)]
 pub struct ChainConfig {
     /// 椭圆曲线，Default Sm2p256v1
     pub curve: Curve,
@@ -24,6 +25,7 @@ pub struct ChainConfig {
 }
 
 /// 连接节点配置
+#[derive(Debug, Clone)]
 pub struct ConnectingNodeConfig {
     /// 节点IP 或者 节点域名
     pub ip: String,
@@ -157,6 +159,37 @@ impl LatticeClient {
             options,
             http_client,
         }
+    }
+
+    /// # 转账
+    ///
+    /// ## 入参
+    /// + `credentials: Credentials`: 身份凭证
+    /// + `chain_id: u64`:
+    /// + `payload: &str`:
+    /// + `amount: Option<u128>`:
+    /// + `joule: Option<u128>`:
+    ///
+    /// ## 出参
+    /// + `Result<String, Error>`
+    pub async fn transfer(&self, credentials: Credentials, chain_id: u64, payload: &str, amount: Option<u128>, joule: Option<u128>) -> Result<String, Error> {
+        let block = self.http_client.get_latest_block(chain_id, &Address::new(credentials.get_account_address().as_str())).await.unwrap();
+
+        let mut transaction = TransferBuilder::builder()
+            .set_current_block(block)
+            .set_owner(credentials.account_address.as_str())
+            .set_linker(ZERO_ZLTC_ADDRESS)
+            .set_payload(payload)
+            .set_amount(amount)
+            .set_joule(joule)
+            .build();
+
+        // Sign transaction
+        let sk = HexString::new(credentials.get_sk().as_str()).decode();
+        let (_, signature) = transaction.sign(chain_id, &sk, self.chain_config.curve);
+        transaction.sign = signature;
+
+        self.http_client.send_raw_tx(chain_id, transaction).await
     }
 
     /// # 部署合约
@@ -307,6 +340,52 @@ mod test {
     ]"#;
 
     const COUNTER_BYTECODE: &str = "0x60806040526000805534801561001457600080fd5b50610278806100246000396000f3fe608060405234801561001057600080fd5b50600436106100415760003560e01c80635b34b96614610046578063a87d942c14610050578063f5c5ad831461006e575b600080fd5b61004e610078565b005b610058610093565b60405161006591906100d0565b60405180910390f35b61007661009c565b005b600160008082825461008a919061011a565b92505081905550565b60008054905090565b60016000808282546100ae91906101ae565b92505081905550565b6000819050919050565b6100ca816100b7565b82525050565b60006020820190506100e560008301846100c1565b92915050565b7f4e487b7100000000000000000000000000000000000000000000000000000000600052601160045260246000fd5b6000610125826100b7565b9150610130836100b7565b9250817f7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff0383136000831215161561016b5761016a6100eb565b5b817f80000000000000000000000000000000000000000000000000000000000000000383126000831216156101a3576101a26100eb565b5b828201905092915050565b60006101b9826100b7565b91506101c4836100b7565b9250827f8000000000000000000000000000000000000000000000000000000000000000018212600084121516156101ff576101fe6100eb565b5b827f7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff018213600084121615610237576102366100eb565b5b82820390509291505056fea2646970667358221220d841351625356129f6266ada896818d690dbc4b0d176774a97d745dfbe2fe50164736f6c634300080b0033";
+    const CHAIN_ID: u64 = 2;
+
+    struct Setup {
+        chain_config: ChainConfig,
+        connecting_node_config: ConnectingNodeConfig,
+        credentials: Credentials,
+        lattice: LatticeClient,
+    }
+
+    impl Setup {
+        fn new() -> Self {
+            let chain_config = ChainConfig {
+                curve: Curve::Sm2p256v1,
+                token_less: true,
+            };
+            let connecting_node_config = ConnectingNodeConfig {
+                ip: String::from("192.168.1.185"),
+                http_port: 13800,
+                websocket_port: 13001,
+            };
+            let credentials = Credentials {
+                sk: String::from("0xdbd91293f324e5e49f040188720c6c9ae7e6cc2b4c5274120ee25808e8f4b6a7"),
+                account_address: String::from("zltc_dS73XWcJqu2uEk4cfWsX8DDhpb9xsaH9s"),
+                passphrase: None,
+                file_key: None,
+            };
+            let lattice = LatticeClient::new(chain_config.clone(), connecting_node_config.clone(), None);
+            // 浅浅青末云顶款
+            Setup {
+                chain_config,
+                connecting_node_config,
+                credentials,
+                lattice,
+            }
+        }
+    }
+
+    #[tokio::test]
+    async fn test_transfer() {
+        let setup = Setup::new();
+        let result = setup.lattice.transfer(setup.credentials, CHAIN_ID, "0x01", None, None).await;
+        match result {
+            Ok(hash) => { println!("转账交易的哈希：{}", hash) }
+            Err(e) => { println!("转账错误，{}", e); }
+        }
+    }
 
     #[tokio::test]
     async fn test_deploy_counter_contract() {
